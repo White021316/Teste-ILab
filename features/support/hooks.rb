aaf2f42190a8f375c8aa317c2f2aba22f52d3e32
Anderson White <anderson.white.bold@bpc.corp>
2020-09-29 22:05:06 -0300

    commit

---
+++ features/support/hooks.rb
@@ -0,0 +1,14 @@
+  
+require_relative 'helper.rb'
+
+After do |cenário|
+  @helper = Helper.new
+
+  @nome = cenário.name.gsub(%r{([_@#!%()\-=;><,{}\~\[\]\./\?\"\*\^\$\+\-]+)}, '')
+
+  @helper.take_screenshot(@nome, 'screenshots/test_failed') if cenário.failed?
+
+  unless @helper.take_screenshot(@nome, 'screenshots/test_passed')
+
+  end
+end
---
+++ features/support/page_objects.rb
@@ -0,0 +1,9 @@
+  
+class Pesquisa < SitePrism::Page
+    element :barra_pesquisa, "input[name='q']"
+    element :pesquisar, "input[name='q']"
+  end
+  class HomePage < SitePrism::Page
+    set_url 'https://www.google.com'
+    end
+    
